44d9a5444197a17dd8476963d67564b75e73c648
vishalbandre <iamvshlbndr@gmail.com>
2022-01-25 10:32:32 +0530

    Header Section

--- web/themes/custom/pop/js/pop.js
+++ web/themes/custom/pop/js/pop.js
@@ -1,130 +1,138 @@
-(function($) {
-
-    $(".region-drawer").hide();
-    document.getElementById("animate-dia").style.transform = "matrix(0.85, 0, 0, 0.85, 0, 0)";
-
-    function openSideDrawer() {
-        document.getElementById("side-drawer").style.left = "0";
-        document.getElementById("side-drawer-void").classList.add("d-block");
-        document.getElementById("side-drawer-void").classList.remove("d-none");
-        $(".region-drawer").show();
-        document.getElementById("animate-dia").classList.add('dia');
-        document.getElementById("animate-dia").style.transform = "matrix(1, 0, 0, 1, 0, 0)";
-    }
-
-    function closeSideDrawer() {
-        document.getElementById("side-drawer").style.left = "-336px";
-        document.getElementById("side-drawer-void").classList.add("d-none");
-        document.getElementById("side-drawer-void").classList.remove("d-block");
-        $(".region-drawer").hide();
-        document.getElementById("animate-dia").style.transform = "matrix(0.85, 0, 0, 0.85, 0, 0)";
-    }
-
-    window.openSideDrawer = openSideDrawer;
-    window.closeSideDrawer = closeSideDrawer;
-
-    // Responsive
-    // $('#mobile-side-drawer').hide();
-
-    // function mobileOpenSideDrawer() {
-    //     console.log('Accessed.');
-    //     document.getElementById("#mobile-side-drawer").style.right = 0;
-    // }
-
-    // function mobileCloseSideDrawer() {
-    //     console.log('Accessed.');
-    // }
-
-    // window.mobileOpenSideDrawer = mobileOpenSideDrawer
-    // window.mobileCloseSideDrawer = mobileCloseSideDrawer
-
-})(jQuery);
-
-
 (function($, window, Drupal) {
-
-    Drupal.behaviors.pop = {
+    Drupal.behaviors.drawer = {
         attach: function(context, settings) {
+
             /**
-             * Mobile drawer component
+             * Drawer component
              */
-            // Get the body object
-            body = document.getElementsByTagName('body')[0];
+            // Set drawer region to hide and apply default tranform matrix with required values.
+            $(".region-drawer").hide();
+            document.getElementById("animate-dia").style.transform = "matrix(0.85, 0, 0, 0.85, 0, 0)";
 
-            // Function to open mobile nav drawer
-            function mobileOpenSideDrawer() {
-                // Set mobile drawer right position to 0. On first load, it is default to -320px;
-                document.getElementById('mobile-side-drawer').style.right = 0;
-                // Show overlay on content, by applying Bootstrap specific classes
-                document.getElementById("mobile-side-drawer-void").classList.add("d-block");
-                document.getElementById("mobile-side-drawer-void").classList.remove("d-none");
-                // Move content to left with ease animation effect.
-                body.style.transition = "ease 0.3s all";
-                body.style.marginLeft = "-320px";
+            // Open main side drawer for desktop navigation
+            function openSideDrawer() {
+                // Set drawer to left 0, i.e., normal state
+                $("#side-drawer").css('left', 0);
+                // Show void overlay by adding display class and removing hide class (Bootstrap).
+                $("#side-drawer-void").addClass("d-block");
+                $("#side-drawer-void").removeClass("d-none");
+                // Show region area and apply animations
+                $(".region-drawer").show();
+                document.getElementById("animate-dia").classList.add('dia');
+                document.getElementById("animate-dia").style.transform = "matrix(1, 0, 0, 1, 0, 0)";
             }
 
-            // Function to close the mobile nav drawer
-            function mobileCloseSideDrawer() {
-                // Set mobile drawer right position to -320px.
-                document.getElementById('mobile-side-drawer').style.right = "-320px";
-                // Hide overlay from content, by applying Bootstrap specific classes
-                document.getElementById("mobile-side-drawer-void").classList.add("d-none");
-                document.getElementById("mobile-side-drawer-void").classList.remove("d-block");
-                // Move content to right, after closing the mobile side drawer
-                body.style.marginLeft = 0;
+            // Close the drawer
+            function closeSideDrawer() {
+                // Pull drawer to minus left position
+                $("#side-drawer").css('left', "-336px");
+                // Hide void overlay by adding hide class and removing display class provided by Bootstrap.
+                $("#side-drawer-void").addClass("d-none");
+                $("#side-drawer-void").removeClass("d-block");
+                // Hide the drawer and set default values (animation specific).
+                $(".region-drawer").hide();
+                document.getElementById("animate-dia").style.transform = "matrix(0.85, 0, 0, 0.85, 0, 0)";
             }
 
-            // Attaching functions to window object to make them available for elements to call
-            context.mobileOpenSideDrawer = mobileOpenSideDrawer
-            context.mobileCloseSideDrawer = mobileCloseSideDrawer
+            $('#open-drawer').click(openSideDrawer);
+            $('#side-drawer-void').click(closeSideDrawer);
+            $('#close-drawer').click(closeSideDrawer);
 
-            // $('.example', context).click(function () {
-            //   $(this).next('ul').toggle('show');
-            // });
+            // Drawer accordion switch (three dot buttons) toggle
+            $(".accordion-content").css("display", "none");
+
+            $(".accordion-switch").click(function() {
+
+                $(".accordion-switch").not(this).removeClass("open");
+                $(".accordion-switch").not(this).next().slideUp(300);
+                $(this).toggleClass("open");
+                $(this).next().slideToggle(300);
+            });
+
         }
     };
 
 })(jQuery, window, Drupal);
 
-// Side Drawer accordion menu switch
-jQuery(function($) {
 
-    $(".accordion-content").css("display", "none");
+(function($, window, Drupal) {
+    Drupal.behaviors.mobile_drawer = {
+        attach: function(context, settings) {
+            /**
+             * Mobile drawer component
+             */
+            // Function to open mobile nav drawer
+            function mobileOpenSideDrawer() {
+                // Set mobile drawer right position to 0. On first load, it is default to -270px;
+                $('#mobile-side-drawer').css('right', 0);
+                // Show overlay on content, by applying Bootstrap specific classes
+                $("#mobile-side-drawer-void").addClass("d-block");
+                $("#mobile-side-drawer-void").removeClass("d-none");
+                // Move content to left with ease animation effect.
+                $("#mobile-actions").addClass("mobile-actions");
+                $('body').addClass("mobile-drawer-set");
+                $('body').removeClass("mobile-drawer-reset");
+            }
+
+            // Function to close the mobile nav drawer
+            function mobileCloseSideDrawer() {
+                // Set mobile drawer right position to -270px.
+                $('#mobile-side-drawer').css('right', "-270px");
+                // Hide overlay from content, by applying Bootstrap specific classes
+                $("#mobile-side-drawer-void").addClass("d-none");
+                $("#mobile-side-drawer-void").removeClass("d-block");
+                // Move content to right, after closing the mobile side drawer
+                $("#mobile-actions").removeClass("mobile-actions");
+                $('body').removeClass("mobile-drawer-set");
+                $('body').addClass("mobile-drawer-reset");
+            }
+
+            // On click events here.
+            $('#mobile-drawer-button').click(mobileOpenSideDrawer);
+            $('#mobile-side-drawer-void').click(mobileCloseSideDrawer);
+            $('#mobile-drawer-close').click(mobileCloseSideDrawer);
+
+        }
+    };
+
+})(jQuery, window, Drupal);
 
 
-    $(".accordion-switch").click(function() {
+(function($, window, Drupal) {
+    Drupal.behaviors.search = {
+        attach: function(context, settings) {
+            /**
+             * Top navbar search component
+             */
+            // Top Search Form Switch
+            $('#search-form-switch').click(function() {
+                $('#header #block-searchform').css("display", "block");
+                $('#header #block-searchform .form-search').focus();
+                $('#header #block-searchform .search-form').append('<a href="#" id="close-search-form"></a>');
+            });
 
-        $(".accordion-switch").not(this).removeClass("open");
+            // Close Search Form
+            $(document).on('click', '#close-search-form', function() {
+                $('#header #block-searchform').css("display", "none");
+            });
 
-        $(".accordion-switch").not(this).next().slideUp(300);
+            /**
+             * Formats text for emphasized display in a placeholder inside a sentence.
+             *
+             * @param {string} str
+             *   The text to format (plain-text).
+             *
+             * @return {string}
+             *   The formatted text (html).
+             */
+            Drupal.theme.indicator = function(str) {
+                return '<li class="' + Drupal.checkPlain(str) + '"></li>';
+            };
 
-        $(this).toggleClass("open");
+            // Global Top Navigation Tweaks
+            $('#superfish-global-top-navigation .menuparent ul').prepend(Drupal.theme('indicator', 'drop-indicator'));
+            $('#superfish-global-top-navigation .menuparent ul').append(Drupal.theme('indicator', 'bottom-indicator'));
+        }
+    };
 
-        $(this).next().slideToggle(300);
-    });
-});
-
-jQuery(function($) {
-    // Top Search Form Switch
-    $('#search-form-switch').click(function() {
-        $('#header #block-searchform').css("display", "block");
-        $('#header #block-searchform .form-search').focus();
-        $('#header #block-searchform .search-form').append('<a href="#" id="close-search-form"></a>');
-    });
-
-    // Close Search Form
-    $(document).on('click', '#close-search-form', function() {
-        $('#header #block-searchform').css("display", "none");
-    });
-
-    // Global Top Navigation Tweaks
-    // Will movie to Drupal.theme
-    $('#superfish-global-top-navigation .menuparent ul').prepend('<li class="drop-indicator"></li>');
-    $('#superfish-global-top-navigation .menuparent ul').append('<li class="bottom-indicator"></li>');
-});
-
-// jQuery(function($) {
-
-// });
-
-// menuparent
+})(jQuery, window, Drupal);
